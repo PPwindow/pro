41b08cabd632b717f2dbe48c6256790b7dae9608
tx <491453227@qq.com>
2019-10-18 19:42:55 +0800

    tx提交

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from './views/Login.vue'
 import tongzhi from './components/tongzhi.vue'
+import txiaoxi from './components/txiaoxi.vue'
 import User from './views/User.vue'
 // 广场组件引入
 import HomeGc from './components/gc/HomeGc.vue'
@@ -11,6 +12,7 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {path:'/User',component:User},
+    {path:'/txiaoxi',component:txiaoxi},
     {path:'/tongzhi',component:tongzhi},
     {path:'/Login',component:Login},
 
