225f5e20bdb1560ac6fe91497e59a82f8c181ac7
pp <pp@qq.com>
2019-10-18 20:02:55 +0800

    错误提交

--- src/router.js
+++ src/router.js
@@ -3,13 +3,10 @@ import Router from 'vue-router'
 import Login from './views/Login.vue'
 import tongzhi from './components/tongzhi.vue'
 import User from './views/User.vue'
-<<<<<<< HEAD
 import Agreement from './views/Agreement.vue'
 import Sign from './views/Sign.vue'
-=======
 // 广场组件引入
 import HomeGc from './components/gc/HomeGc.vue'
->>>>>>> 24a47241a6d7a1e4982364d9f7a336d7bf339e9e
 
 Vue.use(Router)
 
