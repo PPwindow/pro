35151f57c2d5433548c3abf56ab126ead5be2938
pp <pp@qq.com>
2019-10-21 20:31:17 +0800

    pp提交

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,33 +1,16 @@
 <template>
     <div>
-        <div>
-            <a href="">
-                <img src="../assets/user/share_user_banner_black.png" alt="">
-            </a>
-            <a href="">
-                <img src="../assets/user/user_setting_icon_black.png" alt="">
-            </a>
-        </div>
-        <div>
-            <a href="">
-                <img src="" alt="">
-            </a>
-            <a href="">
-                <span>用户名</span>
-            </a>
+        <div class="div_style_big">
+            <div class="div_style_1">
+                <a href="">
+                    <img src="../assets/user/share_user_banner_black.png" alt="">
+                </a>
+                <a href="">
+                    <img src="../assets/user/user_setting_icon_black.png" alt="">
+                </a>
+            </div>
             <div>
-                <a href="">
-                    <span>0</span>
-                    <span>赞与收藏</span>
-                </a>
-                <a href="">
-                    <span>0</span>
-                    <span>粉丝</span>
-                </a>
-                <a href="">
-                    <span>0</span>
-                    <span>关注</span>
-                </a>
+                
             </div>
         </div>
     </div>
@@ -38,5 +21,27 @@ export default {
 }
 </script>
 <style scoped>
-   
+    .div_style_big{
+        margin: 0px 10px;;
+        padding: 0;
+    }
+    .div_style_1{
+        width:355px;
+        height: 30px;
+        margin:10px 0px;
+        position: relative;
+    }
+    .div_style_1 a:first-child{
+        position: absolute;
+        right: 60px;
+    }
+    .div_style_1 a:last-child{
+        position: absolute;
+        right: 15px;
+    }
+    .div_style_1 a img{
+        width: 25px;
+        height: 25px;
+    }
+    
 </style>
